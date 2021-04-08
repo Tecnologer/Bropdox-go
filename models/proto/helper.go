0669001f079c7923a0296ad522d1c1551ab93b0b
Rey David <rdominguez@tecnologer.net>
2021-04-07 23:25:28 -0500

    helper created to build proto objects

---
+++ models/proto/helper.go
@@ -0,0 +1,28 @@
+package proto
+
+import "fmt"
+
+func ParseErrorToResponse(err error) *Response {
+	return CreateErrorResponse(fmt.Sprint(err))
+}
+
+func CreateErrorResponse(message string) *Response {
+	return &Response{
+		Content: &Response_ErrorResponse{
+			ErrorResponse: &ErrorResponse{
+				Message: message,
+			},
+		},
+	}
+}
+
+func CreateFileResponse(file *File, _type TypeResponse) *Response {
+	return &Response{
+		Content: &Response_FileResponse{
+			FileResponse: &FileResponse{
+				File: file,
+				Type: _type,
+			},
+		},
+	}
+}
