e2b64dc1f27a3388d144874c4bf4ed1965b26835
Rey David <rdominguez@tecnologer.net>
2021-04-10 19:29:12 -0500

    client: register for notification and sync files WIP

--- client/main.go
+++ client/main.go
@@ -4,17 +4,21 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"strings"
 	"time"
+	"unicode/utf8"
 
 	log "github.com/sirupsen/logrus"
+	"github.com/tecnologer/bropdox/models/files"
 	"github.com/tecnologer/bropdox/models/proto"
 	"google.golang.org/grpc"
 )
 
 var (
-	verbouse = flag.Bool("v", false, "enanble verbouse log")
-	ip       = flag.String("ip", "", "ip of the server")
-	port     = flag.Int("port", 8081, "port of the server")
+	folderPath = flag.String("path", "./files", "folder to store the files")
+	verbouse   = flag.Bool("v", false, "enanble verbouse log")
+	ip         = flag.String("ip", "", "ip of the server")
+	port       = flag.Int("port", 8081, "port of the server")
 
 	host string
 )
@@ -51,9 +55,43 @@ func main() {
 	filesResponse := response.GetFilesResponse()
 
 	log.Info("Existing files on server: ")
+
+	localFiles, err := files.GetListFileRecursive(*folderPath)
+	if err != nil {
+		log.Warnf("sync files: getting list of local files")
+
+	}
+
+	remoteFiles := []string{}
 	for _, file := range filesResponse.Files {
 		fmt.Printf("\t- %s\n", file.Path)
+
+		remoteFiles = append(remoteFiles, file.Path)
+
+		fileRes, err := client.GetFile(context.Background(), file)
+		if err != nil {
+			log.WithError(err).Warnf("sync files: %s")
+			continue
+		}
+		eRes := fileRes.GetErrorResponse()
+		if eRes != nil {
+			log.Warnf("sync files: trying get file. Response: %s. Path: %s", eRes.Message, file.Path)
+			continue
+		}
+
+		fRes := fileRes.GetFileResponse()
+
+		path := fmt.Sprintf("%s%s", *folderPath, fRes.File.Path)
+		path = strings.Replace(path, "//", "/", -1)
+		err = files.CreateOrUpdate(path, fRes.File.Content)
+		if err != nil {
+			log.WithError(err).Warnf("sync files: downloading file %s", path)
+			continue
+		}
 	}
+
+	compareLocalFilesWRemote(localFiles, remoteFiles)
+
 	notifReq := &proto.NotificationsRequest{
 		Id: fmt.Sprint(time.Now().Unix()),
 	}
@@ -78,7 +116,44 @@ func main() {
 		log.WithFields(log.Fields{
 			"path": fileResponse.File.Path,
 			"type": fileResponse.Type,
-		}).Info("new notification")
+		}).Debug("new notification")
+
+		f := fileResponse.File
+		path := fmt.Sprintf("%s%s", *folderPath, f.Path)
+		switch fileResponse.Type {
+		case proto.TypeResponse_UPDATED:
+			fallthrough
+		case proto.TypeResponse_CREATED:
+			err = files.CreateOrUpdate(path, f.Content)
+			if err != nil {
+				log.WithError(err).Errorf("file notif: %s", fileResponse.Type)
+			}
+		case proto.TypeResponse_DELETED:
+			err = files.Remove(path)
+			if err != nil {
+				log.WithError(err).Errorf("removing file %s", path)
+			}
+		}
 	}
-	log.Infof("started at %s", host)
+}
+
+func compareLocalFilesWRemote(localFiles, remoteFiles []string) {
+	exists := false
+	length := utf8.RuneCountInString(*folderPath)
+	for _, local := range localFiles {
+		exists = false
+		local = local[length-2:]
+		for _, remote := range remoteFiles {
+			if local == remote {
+				exists = true
+				break
+			}
+		}
+
+		if !exists {
+			path := fmt.Sprintf("%s%s", *folderPath, local)
+			path = strings.Replace(path, "//", "/", -1)
+			files.Remove(path)
+		}
+	}
 }
