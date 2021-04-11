ff7edbc01191725de9845cdbe37381ea2b0a00d8
Rey David <rdominguez@tecnologer.net>
2021-04-10 19:17:52 -0500

    update references from file to files

--- server/services/bropdox.go
+++ server/services/bropdox.go
@@ -6,19 +6,21 @@ import (
 	"strings"
 
 	log "github.com/sirupsen/logrus"
-	"github.com/tecnologer/bropdox/models/file"
+	"github.com/tecnologer/bropdox/models/files"
 	"github.com/tecnologer/bropdox/models/proto"
+	"github.com/tecnologer/bropdox/server/models"
 )
 
 var (
-	folderPath = "./files"
-	clients    = map[string]proto.Bropdox_NotificationsServer{}
+	folderPath    = "./files"
+	clients       = models.NewClientCollection()
+	notifications = make(chan *proto.Response, 5)
 )
 
 type BropdoxServer struct{}
 
 func (bs *BropdoxServer) CreateFile(ctx context.Context, in *proto.File) (*proto.Response, error) {
-	err := file.Create(folderPath+"/"+in.Path, in.Content)
+	err := files.CreateOrUpdate(folderPath+"/"+in.Path, in.Content)
 	if err != nil {
 		return proto.ParseErrorToResponse(err), nil
 	}
@@ -27,7 +29,7 @@ func (bs *BropdoxServer) CreateFile(ctx context.Context, in *proto.File) (*proto
 }
 
 func (bs *BropdoxServer) UpdateFile(ctx context.Context, in *proto.File) (*proto.Response, error) {
-	err := file.Create(folderPath+"/"+in.Path, in.Content)
+	err := files.CreateOrUpdate(folderPath+"/"+in.Path, in.Content)
 	if err != nil {
 		return proto.ParseErrorToResponse(err), nil
 	}
@@ -35,7 +37,7 @@ func (bs *BropdoxServer) UpdateFile(ctx context.Context, in *proto.File) (*proto
 }
 
 func (bs *BropdoxServer) RemoveFile(ctx context.Context, in *proto.File) (*proto.Response, error) {
-	err := file.Remove(folderPath + "/" + in.Path)
+	err := files.Remove(folderPath + "/" + in.Path)
 	if err != nil {
 		return proto.ParseErrorToResponse(err), nil
 	}
@@ -43,7 +45,7 @@ func (bs *BropdoxServer) RemoveFile(ctx context.Context, in *proto.File) (*proto
 }
 
 func (bs *BropdoxServer) GetFile(ctx context.Context, in *proto.File) (*proto.Response, error) {
-	file, err := file.Get(folderPath + "/" + in.Path)
+	file, err := files.Get(folderPath + "/" + in.Path)
 	if err != nil {
 		return proto.ParseErrorToResponse(err), nil
 	}
@@ -53,7 +55,7 @@ func (bs *BropdoxServer) GetFile(ctx context.Context, in *proto.File) (*proto.Re
 }
 
 func (bs *BropdoxServer) GetFiles(ctx context.Context, _ *proto.Empty) (*proto.Response, error) {
-	files, err := file.GetListFileRecursive(folderPath)
+	files, err := files.GetListFileRecursive(folderPath)
 	if err != nil {
 		return proto.ParseErrorToResponse(err), nil
 	}
@@ -80,18 +82,18 @@ func (bs *BropdoxServer) Notifications(in *proto.NotificationsRequest, stream pr
 		return fmt.Errorf("the client ID is required")
 	}
 
-	notifications := make(chan *proto.Response, 5)
 	//dir, _ := os.Getwd()
-	err := file.CreateWatcherRecursive(folderPath, notifications)
+	err := files.CreateWatcherRecursive(folderPath, notifications)
 	if err != nil {
 		log.WithError(err).Debug("error creating watcher")
 		return err
 	}
 
 	log.Debugf("new client: %s", in.Id)
-	clients[in.Id] = stream
+	clients.AddNewClient(in.Id, stream)
 
-	defer file.CloseWatchers()
+	//TODO: Improve close watchers
+	//defer files.CloseWatchers()
 
 	go func() {
 		<-stream.Context().Done()
@@ -104,7 +106,7 @@ func (bs *BropdoxServer) Notifications(in *proto.NotificationsRequest, stream pr
 			fileRes.File.Path = strings.Replace(fileRes.File.Path, folderPath, "", 1)
 		}
 
-		for _, stm := range clients {
+		for _, stm := range *clients {
 			stm.Send(notif)
 		}
 	}
